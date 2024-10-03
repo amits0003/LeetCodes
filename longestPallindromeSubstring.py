44bbfc0baa89f7b621265cb9a77e92a84c9b06a1
Amit Kumar <veddaily@gmail.com>
2024-10-03 20:46:57 +0530

    added code to find longest palindrome substring from a string

--- longestPallindromeSubstring.py
+++ longestPallindromeSubstring.py
@@ -12,7 +12,39 @@ Input: s = "cbbd"
 Output: "bb"
 
 """
-s = "ccd"
+t = "ccd"
+
+
+def findPalindromeSubstring(str1):
+    n = len(str1)
+    if n == 0:
+        return ""
+
+    dp = [[False] * n for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = True
+
+    start = 0
+    max_length = 1
+    for i in range(n-1):
+        if str1[i] == str1[i+1]:
+            dp[i][i+1] = True
+            start =  i
+            max_length = 2
+
+    for length in range(3, n+1):
+        for i in range(n-length+1):
+            j = i+length-1
+            if str1[i] == str1[j] and dp[i+1][j-1]:
+                dp[i][j] = True
+                start = i
+                max_length = length
+
+    return str1[start: start+max_length]
+
+
+print(findPalindromeSubstring(t))
 
 
 def palindromeSubstring(s):
@@ -29,4 +61,6 @@ def palindromeSubstring(s):
                 return tempS1
 
 
-print(palindromeSubstring(s))
+# print(palindromeSubstring(s))
+
+
